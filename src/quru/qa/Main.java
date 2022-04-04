73c505110c52e42c47d6a313ed55098bb04bcb2b
djfrolov1996 <djfrolov1996@gmail.com>
2022-04-04 21:43:18 +0300

    numbers to letters

--- src/quru/qa/Main.java
+++ src/quru/qa/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("12345");
         System.out.println("123456");
-        System.out.println("1234567hhhh");
+        System.out.println("1234567hhhh!!!!");
 	// write your code here
     }
 }
